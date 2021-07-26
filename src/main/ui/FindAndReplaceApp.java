2801e6f696b769bb82f21430cd695aeaed2e730a
n1l8q <ashtanm1@students.cs.ubc.ca>
2021-07-25 18:03:47 -0600

    Various UI bug fixes

--- src/main/ui/FindAndReplaceApp.java
+++ src/main/ui/FindAndReplaceApp.java
@@ -4,11 +4,12 @@ import model.Operator;
 import model.Queue;
 import model.exceptions.InvalidLengthException;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-
-// Code for UI adapted from Teller App (from Edx)
+// Code for UI somewhat adapted from Teller App (from Edx)
 public class FindAndReplaceApp {
     private Scanner input;
     private Queue queue;
@@ -45,6 +46,7 @@ public class FindAndReplaceApp {
         System.out.println("\tq -> Queue...");
         System.out.println("\ts -> Strings...");
         System.out.println("\tr -> run operations...");
+        System.out.println("\ta -> about");
         System.out.println("\te -> exit application");
     }
 
@@ -59,8 +61,12 @@ public class FindAndReplaceApp {
             case "r":
                 doRun();
                 break;
+            case "a":
+                doAbout();
+                break;
             default:
                 System.out.println("Invalid key selection");
+                input.nextLine();
                 displayMenu();
                 break;
         }
@@ -69,7 +75,7 @@ public class FindAndReplaceApp {
     private void doRun() {
         System.out.println("\nSelect from:");
         System.out.println("\ta -> Run on all strings");
-        System.out.println("\ti -> Run on an individual string from list");
+        System.out.println("\ti -> Run on an individual string from list...");
         System.out.println("\tn -> Run queue on a new string");
         System.out.println("\tm -> Manual find-and-replace...");
         System.out.println("\th -> Home");
@@ -120,7 +126,7 @@ public class FindAndReplaceApp {
                 System.out.println("Index is larger than size of list");
             } else {
                 try {
-                    operator.iterator(stringLinkedList.get(index), queue);
+                    System.out.println((operator.iterator(stringLinkedList.get(index), queue)));
                 } catch (Exception e) {
                     System.out.println("Unexpected error in iteration");
                 }
@@ -169,11 +175,11 @@ public class FindAndReplaceApp {
 
     private void doQueue() {
         System.out.println("\nSelect from:");
-        System.out.println("\ta -> add new operation...");
-        System.out.println("\to -> edit an operation...");
-        System.out.println("\td -> delete an operation...");
-        System.out.println("\tv -> view queue...");
-        System.out.println("\t h -> home");
+        System.out.println("\ta -> add new operation");
+        System.out.println("\te -> edit an operation");
+        System.out.println("\td -> delete an operation");
+        System.out.println("\tv -> view queue");
+        System.out.println("\th -> home");
         String command = input.next();
         processQueueCommand(command);
     }
@@ -183,7 +189,7 @@ public class FindAndReplaceApp {
             case "a":
                 doAddNewOperation();
                 break;
-            case "o":
+            case "e":
                 doEditOperation();
                 break;
             case "d":
@@ -191,8 +197,10 @@ public class FindAndReplaceApp {
                 break;
             case "v":
                 doViewQueue();
+                doQueue();
                 break;
             case "h":
+                input.nextLine();
                 displayMenu();
                 break;
             default:
@@ -220,6 +228,7 @@ public class FindAndReplaceApp {
 
     private void doEditOperation() {
         try {
+            doViewQueue();
             System.out.println("What is the index of the item you would like to edit?");
             int index = input.nextInt();
             try {
@@ -231,11 +240,11 @@ public class FindAndReplaceApp {
             } catch (InvalidLengthException e) {
                 System.out.println("Queue is not of equal length");
             }
-            doQueue();
         } catch (Exception e) {
             System.out.println("Not an integer. ");
-            doQueue();
+            input.nextLine();
         }
+        doQueue();
     }
 
     private void doEditOperationHelper(int index) throws InvalidLengthException {
@@ -278,7 +287,6 @@ public class FindAndReplaceApp {
         } catch (Exception e) {
             System.out.println("Queue is not of equal length");
         }
-        doQueue();
     }
 
     private boolean checkValid(String newFind, String newReplace, String newReplaceAll) {
@@ -300,9 +308,9 @@ public class FindAndReplaceApp {
 
     private void doStrings() {
         System.out.println("\nSelect from:");
-        System.out.println("\ta -> Add new string...");
-        System.out.println("\td -> delete a string...");
-        System.out.println("\tv -> view strings...");
+        System.out.println("\ta -> Add new string");
+        System.out.println("\td -> delete a string");
+        System.out.println("\tv -> view strings");
         System.out.println("\th -> home");
         String command = input.next();
         processStringCommand(command);
@@ -318,6 +326,7 @@ public class FindAndReplaceApp {
                 break;
             case "v":
                 doViewStrings();
+                doStrings();
                 break;
             case "h":
                 displayMenu();
@@ -332,7 +341,6 @@ public class FindAndReplaceApp {
         for (String s : stringLinkedList) {
             System.out.println(s);
         }
-        doStrings();
     }
 
     private void doDeleteString() {
@@ -349,6 +357,7 @@ public class FindAndReplaceApp {
             }
         } catch (Exception e) {
             System.out.println("Not an integer. ");
+            input.nextLine();
             doStrings();
         }
     }
@@ -370,6 +379,52 @@ public class FindAndReplaceApp {
         queue = new Queue();
         operator = new Operator();
         stringLinkedList = new LinkedList<>();
+    }
+    
+    private void doAbout() {
+        System.out.println("Welcome to the Find and Replace Operator, developed"
+                + " by Ashtan Mistal. Please note: \n \t - Currently, only strings "
+                + "without new lines (\\n) are supported, due to the functionality of Java's input.nextLine() method. "
+                + "\n You can add a list of find and replace operations using the Queue, "
+                + "and perform this queue on multiple pieces of text using the Strings menu. "
+                + "\n \nFor more information, see README.md.");
+        System.out.println("\nSelect from:");
+        System.out.println("\tr -> View README.md");
+        System.out.println("\th -> Home");
+        String command = input.next();
+        processAboutCommand(command);
+    }
+
+    private void processAboutCommand(String command) {
+        switch (command) {
+            case "r":
+                doVisitReadMe();
+                break;
+            case "h":
+                displayMenu();
+                break;
+            default:
+                System.out.println("Invalid key selection, returning home...");
+                displayMenu();
+                break;
+        }
+    }
+
+// File reading abilities from https://stackoverflow.com/a/39026036
+    private void doVisitReadMe() {
+        Scanner input = null;
+        try {
+            input = new Scanner(new File("README.md"));
+        } catch (FileNotFoundException e) {
+            System.out.println("README.md file not found");
+        }
+        while (true) {
+            assert input != null;
+            if (!input.hasNextLine()) {
+                break;
+            }
+            System.out.println(input.nextLine());
+        }
     }
 
 
